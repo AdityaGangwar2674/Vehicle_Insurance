94e8f2e0f6b6b585747b4670f08f7ef3977999ab
Aditya Gangwar <manojkumar1826@rediffmail.com>
2025-04-25 22:50:25 +0530

    changes in claim

--- backend/controllers/claimController.js
+++ backend/controllers/claimController.js
@@ -1,14 +1,24 @@
 const Claim = require("../models/ClaimModel");
+const Insurance = require("../models/InsuranceModel");
 
 const createClaim = async (req, res) => {
   const { insurance, incidentDate, description, amountClaimed } = req.body;
+
   try {
+    const insurancePolicy = await Insurance.findById(insurance);
+    if (!insurancePolicy) {
+      return res.status(404).json({
+        success: false,
+        message: "Insurance policy not found.",
+      });
+    }
     const newClaim = await Claim.create({
       insurance,
       incidentDate,
       description,
       amountClaimed,
     });
+
     res.status(201).json({
       success: true,
       data: newClaim,
@@ -17,6 +27,7 @@ const createClaim = async (req, res) => {
     res.status(500).json({
       success: false,
       message: "Server error while creating claim.",
+      error: error.message,
     });
   }
 };
