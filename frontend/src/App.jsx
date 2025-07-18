1d51dd1455dbd273abec4a0a8e2acd3ccc0cf989
Aditya Gangwar <manojkumar1826@rediffmail.com>
2025-07-18 21:22:05 +0530

    basic layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/user/Dashboard";
+import Claims from "./pages/user/Claims";
+import Insurance from "./pages/user/Insurance";
+import Payment from "./pages/user/Payment";
+import Vehicles from "./pages/user/Vehicles";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import Customers from "./pages/admin/Customers";
+import CustomerClaims from "./pages/admin/CustomerClaims";
+import CustomerPayments from "./pages/admin/CustomerPayments";
+
+function App() {
+  return (
+    <div className="">
+      <Router>
+        <Routes>
+          {/* main page */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* dashboard page */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/claims" element={<Claims />} />
+          <Route path="/insurance" element={<Insurance />} />
+          <Route path="/payments" element={<Payment />} />
+          <Route path="/vehicles" element={<Vehicles />} />
+
+          {/* admin dashboard */}
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/customers" element={<Customers />} />
+          <Route path="/admin/claims" element={<CustomerClaims />} />
+          <Route path="/admin/payments" element={<CustomerPayments />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/admin/CustomerClaims.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomerClaims = () => {
+  return <div>CustomerClaims</div>;
+};
+
+export default CustomerClaims;
